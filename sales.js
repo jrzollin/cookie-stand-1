92f8f2375430e6fb6d3ae1c0cf75ef1d1e958df6
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-06-10 16:32:04 -0700

    enough css for a day

--- sales.js
+++ sales.js
@@ -22,8 +22,6 @@ function tableScaffold(){
   //created array below so I can set-up headings of table
   var hoursOpen = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
-  //setting up the title row of my table outside of the loop since I only need it once
-
   //first I set up the table header row with proper titles
   var tableHeader = document.getElementById('table-hours'); //grabbed the <thead> element from my html
   var tableHeaderRow = document.createElement('tr');  //created a <tr> so I could loop and append <td> to it
